42985d606a60698101a88d988f953384a4725f83
Maile Soon Fah <maile.soonfah23@ycdsbk12.ca>
2021-01-25 14:05:52 +0000

    main.py why does question one get stuck?

--- main.py
+++ main.py
@@ -43,61 +43,61 @@ print()
 
 # The boy route
 if char == "boy":
-      print("A blur jumps out and lunges at your charater driving their sword into it's stomach. You roll your swivel chair (aka spinny chair) backwards from surprize wondering if this game really is buggy. You turn around the chair wondering if you saw what happened corretly when you hear a loud thump behind you.")
+  print("A blur jumps out and lunges at your charater driving their sword into it's stomach. You roll your swivel chair (aka spinny chair) backwards from surprize wondering if this game really is buggy. You turn around the chair wondering if you saw what happened corretly when you hear a loud thump behind you.")
+
+  print()
+
+  # The start of many options for the boy side of the story. (Don't mind the multiple print statements. It's confusing I know.)
+  option_1 = input("Do you 'turn around' or do you 'stay': ")
+  print()
+
+  if option_1 == "turn around":
+    print("???: Okay foolish ghost where did you send me.")
+    print("*He holds a blade towards you*.")
+    print("???: Who are you? Are you an apprentice to that wall phasing fool?")
+
+    print()
+    print("What do you want to say to him?")
+    print("'who're you calling wall phasing fool!' ")
+    option_2 = input("or 'what do you mean?': ")
+    print()
+
+    if option_2 == "who're you calling wall phasing fool!":
+      print("???: You...You're the ghost who keeps scaring millions of people in my town that it's so unbearably quiet. It's practically a ghost town!")
 
       print()
 
-      # The start of many options for the boy side of the story. (Don't mind the multiple print statements. It's confusing I know.)
-      option_1 = input("Do you 'turn around' or do you 'stay': ")
-      print()
-
-      if option_1 == "turn around":
-        print("???: Okay foolish ghost where did you send me.")
-        print("*He holds a blade towards you*.")
-        print("???: Who are you? Are you an apprentice to that wall phasing fool?")
+      print("*There is a hissing and buzzing sound coming from your computer.*")
 
       print()
-      print("What do you want to say to him?")
-      print("'who're you calling wall phasing fool!' ")
-      option_2 = input("or 'what do you mean?': ")
+
+      print("???:What is that doing?")
+
       print()
 
-      if option_2 == "who're you calling wall phasing fool!":
-        print("???: You...You're the ghost who keeps scaring millions of people in my town that it's so unbearably quiet. It's practically a ghost town!")
+      print("*He walks over cautiously and pokes the monitor with his blade You look concerned over to your amazing monitor you've played on for 5 years so far.*")
 
-        print()
+      print()
 
-        print("*There is a hissing and buzzing sound coming from your computer.*")
+      print("???:What is this annoying sou-")
 
-        print()
+      print()
 
-        print("???:What is that doing?")
+      print("*The monitor bursts into flames. You dread over the fact that the monitor was amazing an lasted for so long* (RIP monitor from the beginning of this game - now. Forgot I was here did you :3) *The boy panics and completely freezes the monitor and your tower.* (The pain continues.) *You sit and stare at the now ruined computer.*")
 
-        print()
+      print()
 
-        print("*He walks over cautiously and pokes the monitor with his blade You look concerned over to your amazing monitor you've played on for 5 years so far.*")
-
-        print()
-
-        print("???:What is this annoying sou-")
-
-        print()
-
-        print("*The monitor bursts into flames. You dread over the fact that the monitor was amazing an lasted for so long* (RIP monitor from the beginning of this game - now. Forgot I was here did you :3) *The boy panics and completely freezes the monitor and your tower.* (The pain continues.) *You sit and stare at the now ruined computer.*")
-
-        print()
-
-        print("???:Ah- Sorry I didn't mean to- Wait why am I apologizing to you! How did I get here!?")
+      print("???:Ah- Sorry I didn't mean to- Wait why am I apologizing to you! How did I get here!?")
       
-        print()
+      print()
 
-        print("*The boy holds his blade toward you again.*")
+      print("*The boy holds his blade toward you again.*")
 
-        print()
+      print()
 
-        option_3 = input("'shrug' or 'ignore and continue to sulking': ")
+      option_3 = input("'shrug' or 'ignore and continue to sulking': ")
 
-        print()
+      print()
 
       if option_3 == "shrug":
         print("???:*sigh*")
@@ -190,130 +190,130 @@ if char == "boy":
 
 
 
-      elif option_1 == "stay":
-        print("???: Hey you! Don't ignore me where am I?")
+  elif option_1 == "stay":
+    print("???: Hey you! Don't ignore me where am I?")
+
+    print()
+
+    option_2 = input("What do you want to say to him? 'who and what are you?' or do you want to 'panic and throw a book at him'")
+
+    print()
+
+    if option_2 == "who and what are you":
+      print("???: What do you mean who and what are you? My name is " + char_name + " and I'm a human. Now I ask. Who and what are you?")
 
       print()
 
-      option_2 = input("What do you want to say to him? 'who and what are you?' or do you want to 'panic and throw a book at him'")
+      print("*You introduce yourself and say that your human too*")
 
       print()
 
-      if option_2 == "who and what are you":
-        print("???: What do you mean who and what are you? My name is " + char_name + " and I'm a human. Now I ask. Who and what are you?")
+      print(char_name + ":" + mc + "...That's nice. So what am I doing here?")
 
-        print()
+      print()
 
-        print("*You introduce yourself and say that your human too*")
+      print("*You shrug*")
 
-        print()
+      print()
 
-        print(char_name + ":" + mc + "...That's nice. So what am I doing here?")
+      print(char_name + ": Huh...I guess I'm stu-")
 
-        print()
+      print()
 
-        print("*You shrug*")
+      print("*There is a hissing and buzzing sound coming from your computer.*")
 
-        print()
+      print(char_name + ": What is that sound?")
 
-        print(char_name + ": Huh...I guess I'm stu-")
+      print()
 
-        print()
+      print("*The monitor bursts into flames. You dread over the fact that the monitor was amazing an lasted for so long* (RIP monitor from the beginning of this game - now. Forgot I was here did you :3) *" + char_name + " panics and completely freezes the monitor and your tower.* (The pain continues.) *You sit and stare at the now ruined computer.*")
 
-        print("*There is a hissing and buzzing sound coming from your computer.*")
+      print()
 
-        print(char_name + ": What is that sound?")
+      print(char_name + ": Well the fire is out...Why are you sulking like that? Was the contraption really that important?")
 
-        print()
+      print()
 
-        print("*The monitor bursts into flames. You dread over the fact that the monitor was amazing an lasted for so long* (RIP monitor from the beginning of this game - now. Forgot I was here did you :3) *" + char_name + " panics and completely freezes the monitor and your tower.* (The pain continues.) *You sit and stare at the now ruined computer.*")
+      print("*You sigh and drop his blade to the ground and drag " + char_name + " by the arm to a nearby computer store.* (Multiple choice quiz time.)")
 
-        print()
+      print()
 
-        print(char_name + ": Well the fire is out...Why are you sulking like that? Was the contraption really that important?")
+      # Multiple choice quiz
+      print(char_name + ": Oi! Where are you dragging me- What are these things?")
 
-        print()
+      print()
 
-        print("*You sigh and drop his blade to the ground and drag " + char_name + " by the arm to a nearby computer store.* (Multiple choice quiz time.)")
+      # Mini game 
+      print("Okay so mini game time. Are you going to explain all things computers? No that would be too much so this is going to be a small multiple question quiz. Try your best to answer or you'll have to do the story all over again. All you need to do is...")
 
-        print()
+      print()
 
-        # Multiple choice quiz
-        print(char_name + ": Oi! Where are you dragging me- What are these things?")
+      print("TYPE THE LETTER <-Don't forget")
 
-        print()
+      print()
 
-        # Mini game 
-        print("Okay so mini game time. Are you going to explain all things computers? No that would be too much so this is going to be a small multiple question quiz. Try your best to answer or you'll have to do the story all over again. All you need to do is...")
-
-        print()
-
-        print("TYPE THE LETTER <-Don't forget")
-
-        print()
-
-        print("How does a mouse convert distance for display?")
+      print("How does a mouse convert distance for display?")
       
-        print()
+      print()
         
-        print("A. DPI (dots per itch)")
-        print("B. DPI (dots per inch)")
-        print("C. DPS (diameter perimeter speed")
-        print("D. PAD (perimeter area distance")
+      print("A. DPI (dots per itch)")
+      print("B. DPI (dots per inch)")
+      print("C. DPS (diameter perimeter speed")
+      print("D. PAD (perimeter area distance")
 
-        question_1 = input("Which letter is correct: ")
+      question_1 = input("Which letter is correct: ")
+
+      print()
+
+      if question_1 == "B":
+        print("Correct! On to the next question.")
+      else:
+        print("Oof! You have been dead ended due to incorrect answer.")
 
         print()
 
-        if question_1 == "B":
-          print("Correct! On to the next question.")
+        print("Question two. What is the smallest and fastest data storage on a computer?")
+        print()
+
+        print("A. Cashe")
+        print("B. Ram")
+        print("C. Hard Drive")
+
+        question_2 = input("Which letter is correct: ")
+          
+        print()
+
+        if question_2 == "A":
+          print("Correct! On to the last question.")
         else:
           print("Oof! You have been dead ended due to incorrect answer.")
 
           print()
 
-          print("Question two. What is the smallest and fastest data storage on a computer?")
+          print("Last and final question.")
+          print("What is the refresh on a computer monitor measured in?")
+
           print()
 
-          print("A. Cashe")
-          print("B. Ram")
-          print("C. Hard Drive")
+          print("A. Hurtz")
+          print("B. Fartz")
+          print("C. Hearts")
+          print("D. Hertz")
 
-          question_2 = input("Which letter is correct: ")
-          
+          question_3 = input("Which letter is correct?")
+
           print()
 
-          if question_2 == "A":
-            print("Correct! On to the last question.")
-          else:
-            print("Oof! You have been dead ended due to incorrect answer.")
-
-            print()
-
-            print("Last and final question.")
-            print("What is the refresh on a computer monitor measured in?")
-
-            print()
-
-            print("A. Hurtz")
-            print("B. Fartz")
-            print("C. Hearts")
-            print("D. Hertz")
-
-            question_3 = input("Which letter is correct?")
-
-            print()
-
-            if question_3 == "D":
-              print("Congratulations! You've completed the game!")
-              print("...")
-              print("For now...")
+          if question_3 == "D":
+            print("Congratulations! You've completed the game!")
+            print("...")
+            print("For now...")
             
-            elif question_3 == "C":
-              print("You were really that desperate? (Sorry for the insensitive joke.)")
+          elif question_3 == "C":
+            print("You were really that desperate? (Sorry for the insensitive joke.)")
 
-            else:
-              print("You were so close. Sorry to say this but you lost. Try a different answer next time!")
+          else:
+            print("You were so close. Sorry to say this but you lost. Try a different answer next time!")
 
 
 
